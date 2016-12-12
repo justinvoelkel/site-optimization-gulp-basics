7112d80c796a75dd0469a17a0eda96946ab2a825
Justin Voelkel <justin@budgetdumpster.com>
2016-12-12 15:49:04 -0500

    adding in foundation js files, updating gulp file, and adding some images

--- gulpfile.js
+++ gulpfile.js
@@ -15,11 +15,9 @@ gulp.task('styles',function(){
         './styles/development/scss/**/*.scss'
     ];
     return gulp.src(stylesSrc)
-        //.pipe(concat('main.scss'))
-        //.pipe(gulp.dest('./styles/development/'))
         .pipe(sass())
         .pipe(concat('main.css'))
-        .pipe(gulp.dest('./styles/development/'))
+        .pipe(gulp.dest('./styles/production/'))
         .pipe(rename('main.min.css'))
         .pipe(minifyCSS())
         .pipe(gulp.dest('./styles/production/'));
@@ -27,39 +25,28 @@ gulp.task('styles',function(){
 
 gulp.task('scripts',function(){
     var scriptSrc =[
-        'assets/js/development/lib/jquery.min.js',
-        'assets/js/development/lib/bootstrap.min.js',
-        'assets/js/development/lib/autocomplete.js',
-        'assets/js/development/lib/starRating.js',
-        'assets/js/development/lib/yt-modal-bug.js',
-        'assets/js/development/lib/mobile-nav.js',
-        'assets/js/development/lib/on-load.js'
+        './js/development/vendor/**/*.js',
+        './js/development/foundation/**/*.js',
+        './js/development/app.js'
 
     ];
     gulp.src(scriptSrc)
-        .pipe(concat('production.js'))
-        .pipe(gulp.dest('assets/js/production/'))
-        .pipe(rename('production.min.js'))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./js/production/'))
+        .pipe(rename('main.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('assets/js/production/'))
+        .pipe(gulp.dest('./js/production/'));
 });
 
 gulp.task('images',function(){
-    return gulp.src('../images/*')
-        .pipe(imagemin({
-            progressive:true,
-            svgPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest('../images'));
+    return gulp.src('./images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./images'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('assets/styles/development/**/*.css',['styles']);
-    gulp.watch(
-        ['assets/js/development/**/*.js','!assets/js/production/*.js'],
-        ['scripts']
-    );
+    gulp.watch('./styles/development/**/*.css', ['styles']);
+    gulp.watch('./js/development/**/*.js', ['scripts']);
 });
 
 gulp.task('default', ['styles','scripts','images']);
